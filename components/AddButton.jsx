37e6b80c1eff8cba8726146da4b035e4852d5570
KarouiYassine <95907031+KarouiYassine@users.noreply.github.com>
2023-05-31 22:59:37 +0200

    Delete components directory

--- components/AddButton.jsx
+++
@@ -1,8 +0,0 @@
-import { Button } from 'react-native'
-import React from 'react'
-
-export default function AddButton(props) {
-    return (
-        <Button color="skyblue" title={props.content} onPress={props.onClick} />
-    )
-}
