004bd796f9455003179c1e998d9e67087b5816bd
fayezkaabi26 <67579975+fayezkaabi26@users.noreply.github.com>
2023-06-01 10:09:37 +0200

    Update MovieModal.jsx

--- components/MovieModal.jsx
+++ components/MovieModal.jsx
@@ -1,59 +1,79 @@
-import { View, Text, Button, StyleSheet, Modal } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, StyleSheet, Modal, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
 import MovieForm from './MovieForm';
 import AddButton from './AddButton';
 import { addMovie, updateMovie } from '../Fire';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function MovieModal(props) {
-    console.log(props.isVisible);
-    const [title, setTitle] = useState(props.editMovie ? props.editMovie.title : "");
-    const [synopsis, setSynopsis] = useState(props.editMovie ? props.editMovie.synopsis : "");
-    const [image, setImage] = useState(props.editMovie ? props.editMovie.image : "");
+  const [title, setTitle] = useState(props.editMovie ? props.editMovie.title : '');
+  const [synopsis, setSynopsis] = useState(props.editMovie ? props.editMovie.synopsis : '');
+  const [image, setImage] = useState(props.editMovie ? props.editMovie.image : '');
 
-    const handleSubmit = () => {
-        let movie = {
-            "title": title,
-            "synopsis": synopsis,
-            "image": image,
-            "comments": []
-        }
-        if (props.editMovie) {
-            movie.id = props.editMovie.id
-            movie.comments = props.editMovie.comments
-            updateMovie(movie)
-        } else {
-            addMovie(movie)
-        }
-        props.onClose()
+  const handleSubmit = () => {
+    let movie = {
+        "title": title,
+        "synopsis": synopsis,
+        "image": image,
+        "comments": []
     }
+    if (props.editMovie) {
+        movie.id = props.editMovie.id
+        movie.comments = props.editMovie.comments
+        updateMovie(movie)
+    } else {
+        addMovie(movie)
+    }
+    props.onClose()
+}
 
-    return (
-        <Modal style={styles.modal} visible={props.isVisible}>
-            <View style={styles.background}>
-                <MovieForm title={title} synopsis={synopsis} image={image} handleTitleChange={newTitle => setTitle(newTitle)} handleSynopsisChange={newSynopsis => setSynopsis(newSynopsis)} handleImageChange={newImage => setImage(newImage)} />
+  return (
+    <Modal visible={props.isVisible} style={styles.modal}>
+      <View style={styles.background}>
+        <MovieForm
+          title={title}
+          synopsis={synopsis}
+          image={image}
+          handleTitleChange={(newTitle) => setTitle(newTitle)}
+          handleSynopsisChange={(newSynopsis) => setSynopsis(newSynopsis)}
+          handleImageChange={(newImage) => setImage(newImage)}
+        />
 
-                <View style={styles.button}><AddButton content="Valider" onClick={handleSubmit} /></View>
-                <View style={styles.button}><AddButton content="Fermer" onClick={props.onClose} /></View>
-            </View>
-        </Modal>
-    )
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.iconButton} onPress={handleSubmit}>
+            <Ionicons name="checkmark" size={40} color="lightgreen" />
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.iconButton} onPress={props.onClose}>
+            <Ionicons name="close" size={40} color="red" />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
+  );
 }
 
 const styles = StyleSheet.create({
-    modal: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'black'
-    },
-    button: {
-        marginBottom: 5,
-        alignItems: 'center',
-    },
-    background: {
-        flex: 1,
-        backgroundColor: 'black'
-    },
-    text: {
-        color: 'white',
-    },
+  modal: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+  },
+  iconButton: {
+    alignItems: 'center',
+    marginRight: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginBottom: 5,
+  },
+  background: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+  },
 });
